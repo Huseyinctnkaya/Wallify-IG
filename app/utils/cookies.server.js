206931a35a482033e50b38ecd9c42395e389912f
Hüseyin Çetinkaya <huseyinctnkaya12@gmail.com>
2026-02-18 17:06:15 +0300

    Revert "instagram cancel button"
    This reverts commit 87068ff07f4fae40080872a1910c5abea56a2035.

--- app/utils/cookies.server.js
+++
@@ -1,8 +0,0 @@
-import { createCookie } from "@remix-run/node";
-
-export const igAuthShopCookie = createCookie("ig_auth_shop", {
-    maxAge: 600, // 10 minutes
-    httpOnly: true,
-    secure: true,
-    sameSite: "lax",
-});
